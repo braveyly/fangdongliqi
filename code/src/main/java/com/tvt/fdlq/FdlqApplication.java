728948c995e940880422adca0dcb86af45dd338b
liwei <liwei@tvt.net.cn>
2018-10-27 16:39:18 +0800

    init project and generate domain mapper etc.

---
+++ code/src/main/java/com/tvt/fdlq/FdlqApplication.java
@@ -0,0 +1,12 @@
+package com.tvt.fdlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdlqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdlqApplication.class, args);
+    }
+}
---
+++ code/src/test/java/com/tvt/fdlq/FdlqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tvt.fdlq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FdlqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
